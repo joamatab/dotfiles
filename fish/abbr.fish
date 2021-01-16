9d28771d35bb7be4290000ce79f3285cbf9ecfe8
joaquin <j>
2021-01-15 23:28:56 -0800

    more fzf

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -26,7 +26,7 @@ if status --is-interactive
     abbr -a dud 'killall -SIGUSR1 dunst'
     abbr -a e exa --git-ignore -I __pycache__
     abbr -a et exa --tree --git-ignore -I __pycache__
-    abbr -a et export TERM=xterm-256color
+    abbr -a eterm export TERM=xterm-256color
     abbr -a f fzf
     abbr -a is "find . -type l | sed -e s'/^\.\///g' >> .gitignore"
     abbr -a kus "setxkbmap -layout us"
