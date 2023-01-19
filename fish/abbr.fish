96bc0828b1534756a373c94ed6805828ee9832f6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-18 17:09:01 -0800

    git lfs

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -75,9 +75,7 @@ if status --is-interactive
     abbr -a timemadrid 'timedatectl set-timezone Europe/Madrid'
     abbr -a te terraform
     abbr -a v nvim
-    abbr -a vi nvim
-    abbr -a vim nvim
-    abbr -a vpn  'nmcli con up vpno 2>&1'
+    abbr -a vpn 'nmcli con up vpno 2>&1'
     abbr -a ww nvim -c "VimwikiIndex"
     abbr -a wi nvim -c "VimwikiDiaryIndex"
     abbr -a wa nvim  ~/wikis/awesome_photonics/README.md
