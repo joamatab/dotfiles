2ce0c56e5c2e010f655eafb3909c140870b72ea9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-07-30 09:17:18 -0700

    simpler tmux

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -4,19 +4,17 @@ if status --is-interactive
     abbr -a bm nvim ~/.bmdirs
     abbr -a d cd ~/Downloads
     abbr -a ca 'eval ~/.pyenv/versions/miniconda3-latest/bin/conda "shell.fish" "hook" $argv | source'
-    abbr -a cfa nvim ~/dotfiles/fish/abbr.fish
-    abbr -a cfb nvim ~/dotfiles/bashrc
-    abbr -a cff nvim ~/dotfiles/fish/config.fish
+    abbr -a cfa nvim ~/.config/fish/abbr.fish
+    abbr -a cfb nvim ~/.bashrc
+    abbr -a cff nvim ~/.config/fish/config.fish
     abbr -a cffl nvim ~/.local/share/fish/config.fish
-    abbr -a cfi nvim ~/dotfiles/i3/config
-    abbr -a cfk nvim ~/dotfiles/kitty.conf
     abbr -a cfl nvim ~/dotfiles/lf/lfrc
     abbr -a cfr nvim ~/dotfiles/ranger/rc.conf
     abbr -a cfs nvim ~/.config/sxhkd/sxhkdrc
     abbr -a cfsw nvim ~/.config/sway/config
     abbr -a cfssh nvim ~/.ssh/config
-    abbr -a cft nvim ~/dotfiles/tmux.conf.local
-    abbr -a cfv nvim ~/dotfiles/nvim/init.vim
+    abbr -a cft nvim ~/.tmux.conf
+    abbr -a cfv nvim ~/.config/nvim/init.vim
     abbr -a cfy nvim ~/.yabairc
     abbr -a cfx nvim ~/.xonshrc
     abbr -a cfz nvim ~/.zshrc
--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
 ./install
-
-
-if [ ! -d $HOME/.tmux ]; then
-  cd
-  git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
-  ln -s -f .tmux/.tmux.conf
-  cp .tmux/.tmux.conf.local .
-fi
-
 [ ! -d luke ] && git clone https://github.com/LukeSmithxyz/voidrice.git luke
 ln -s $PWD/luke/.local/share/larbs ~/.local/share/larbs
 cp luke/.local/share/thiemeyer_road_to_samarkand.jpg ~/.local/share/bg
