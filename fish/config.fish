bce2be0ad812073f0f492a8656ef2ba28625a838
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-09-14 16:10:48 +0200

    remove pre-commit color

--- fish/config.fish
+++ fish/config.fish
@@ -256,3 +256,4 @@ end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/j/google-cloud-sdk/path.fish.inc' ]; . '/Users/j/google-cloud-sdk/path.fish.inc'; end
+set -gx PRE_COMMIT_COLOR never
