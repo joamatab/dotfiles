726611dfe4f244d0e705ed66ebc5349643e15af9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-04-11 07:52:04 -0700

    keep lfcd and add yazicd

--- fish/functions.fish
+++ fish/functions.fish
@@ -125,6 +125,20 @@ end
 
 function lfcd
     set tmp (mktemp)
+    lf -last-dir-path=$tmp $argv
+    if test -f "$tmp"
+        set dir (cat $tmp)
+        rm -f $tmp
+        if test -d "$dir"
+            if test "$dir" != (pwd)
+                cd $dir
+            end
+        end
+    end
+end
+
+function yazicd
+    set tmp (mktemp)
     yazi --cwd-file=$tmp $argv
     if test -f "$tmp"
         set dir (cat $tmp)
